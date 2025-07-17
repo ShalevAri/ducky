c6969e784f96747ab5edafc50ce1d9399b73e887
shalev <shalevari2008@gmail.com>
2025-07-17 13:30:56 +0300

    feat: remove testing logic

--- tests/ducklings/github-duckling.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('should redirect to GitHub when query is "github"', async ({ page }) => {
-  await page.goto('http://localhost:49152/ducky/?q=github')
-  expect(page.url()).toBe('https://github.com/')
-})
